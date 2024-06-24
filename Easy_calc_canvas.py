0e9fd49cd2f71a6818b83059e26a2b26e5ff7f07
Aditya Sinha <92170969+Fulmen-Draco@users.noreply.github.com>
2024-06-24 11:49:10 +0530

    Update Easy_calc_canvas.py
    added support for ^

--- Easy_calc_canvas.py
+++ Easy_calc_canvas.py
@@ -14,7 +14,7 @@ def extract_text_from_image(image):
 
 def find_equation(text):
     # find all instances of arithmetic expressions
-    equation_pattern = re.compile(r'[\d\+\-\*\/\(\)\s]+')
+    equation_pattern = re.compile(r'[\d\+\-\*\/\(\)\^\s]+')
     matches = equation_pattern.findall(text)
     # Join the matches to form the equation string
     if matches:
@@ -24,6 +24,8 @@ def find_equation(text):
         return None
 
 def format_equation(equation):
+    # replace ^ with **
+    equation = equation.replace('^','**')
     # Inserting multiplication operator where a number is directly followed by a parenthesis
     equation = re.sub(r'(\d)(\()', r'\1*\2', equation)
     return equation
